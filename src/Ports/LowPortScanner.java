44082358e0dbb2d4f4d42d1bdfda7e5063f1e835
finnhuelsbusch <f.huelsbusch@outlook.de>
2020-10-20 08:49:03 +0200

    fixed portscanner

--- src/Ports/LowPortScanner.java
+++ src/Ports/LowPortScanner.java
@@ -1,25 +1,26 @@
 package Ports;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class LowPortScanner {
 
-    public static void main(String[] args) throws UnknownHostException {
-        InetAddress adr = InetAddress.getLocalHost();
-
-        for ( int i = 40000; i < 65536; i++ ) {
+    public static void main(String[] args) {
+        String host = "localhost";
+        if (args.length > 0) {
+            host = args[0];
+        }
+        for (int i = 0; i < 65536; i++) {
             try {
-                Socket s = new Socket(adr, i);
-                System.out.printf("There is a Server on port %d at host %s%n", i, adr);
+                Socket s = new Socket(host, i);
+                System.out.printf("There is a Server on port %d at host %s%n", i, host);
                 s.close();
             } catch (UnknownHostException e) {
                 System.err.println(e);
                 break;
             } catch (IOException e) {
-                e.printStackTrace();
+
             }
         }
     }
